2ba7e2bb7f8e1a2692eaa8359a9cf87199ca8f19
たまご <33786273+INPNOffice@users.noreply.github.com>
2018-08-25 19:38:04 +0900

    進捗:スマホで全画面表示まで可能(回転は未実装)

--- sources/htmlWrap.js
+++ sources/htmlWrap.js
@@ -1,29 +1,41 @@
 var bod = document.getElementsByTagName("body")[0];
 var cvs = new ARatio(document.getElementsByTagName("canvas")[0]);
-console.log(cvs);
 var defltcvs = cvs.dObject.style;
 var tofs = function(){
-	cvs.dObject.removeEventListener("click", exit);
+	console.log("クリックを捕捉");
 	bod.mozRequestFullScreen();
-	setTimeout(function(){
-		var fbody = new ARatio(bod);
-		console.log(bod.clientWidth, bod.clientHeight);
-		var c = fbody.comp(cvs);
-		console.log(c);
-		cvs.dObject.style[c.ftgt]="100%";
-		cvs.dObject.style[c.fths]="";
-		document.addEventListener("fullscreenchange", exit);
-	}, 2000);
+	document.addEventListener(
+		"mozfullscreenchange",
+		function(){
+			bod.removeEventListener("click", tofs);
+			console.log("clickイベントリスナを削除");
+			var fbody = new ARatio(bod);
+			console.log(bod.clientWidth, bod.clientHeight);
+			var c = fbody.comp(cvs);
+			console.log(c);
+			cvs.dObject.style[c.ftgt]="100%";
+			cvs.dObject.style[c.fths]="";
+			document.addEventListener("mozfullscreenchange", exit);
+		},
+		{
+			"capture":false,
+			"once":true
+		}
+	);
 };
 var exit = function(){
+	console.log("もどした");
 	cvs.dObject.style = defltcvs;
-	document.removeEventListener("fullscreenchange", exit);
-	cvs.dObject.addEventListener("click", tofs);
+	document.removeEventListener("mozfullscreenchange", exit);
+	bod.addEventListener("click", tofs);
 };
-cvs.dObject.addEventListener("click", tofs);
+bod.addEventListener("click", tofs);
+console.log("キャンバス準備完了");
 
 var RotateForSP = function(event){
-	alert(event.beta);
+	console.log(event.beta);
+	window.removeEventListener("deviceorientation", RotateForSP);
+	setTimeout(function(){window.addEventListener("deviceorientation", RotateForSP);}, 500);
 };
 //スマホの画面回転対応
 window.addEventListener("deviceorientation", RotateForSP);
--- sources/iframe.js
+++ sources/iframe.js
@@ -1,6 +1,7 @@
 var gf = document.getElementById("game_frame");
 if(gf !== null){
 	gf.allowFullscreen = true;
+	console.log("フレーム1準備完了");
 }
 htmlWrap();
 
@@ -9,6 +10,7 @@ function htmlWrap(){
 	switch(true){
 		case hW != null:
 			hW.allowFullscreen = true;
+			console.log("フレーム2準備完了");
 			break;
 		case document.getElementById("flashWrap") != null:
 			setTimeout(htmlWrap, 500);
