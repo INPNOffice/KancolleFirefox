7a38d4b4d05e00c2c6315f27b591c1fd60db52f5
naari3 <naari.named@gmail.com>
2018-08-26 02:41:23 +0900

    Change webpack config to suitable

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,32 @@
 const webpack = require("webpack");
 const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: process.env.NODE_ENV || "development",
-  entry: ['./index.js'],
+  entry: {
+    htmlWrap: path.resolve(
+      __dirname,
+      "src",
+      "scripts",
+      "htmlWrap.js"
+    ),
+    iframe: path.resolve(
+      __dirname,
+      "src",
+      "scripts",
+      "iframe.js"
+    )
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.js"
+    filename: "scripts/[name].bundle.js"
   },
   target: "web",
   loader: {
     rules: [
       {
-        test: /\.(js|tag)$/,
+        test: /\.(js)$/,
         use: "babel-loader",
         enforce: "post",
         exclude: /node_modules/
@@ -22,5 +36,22 @@ module.exports = {
   devServer: {
     port: 8080,
     contentBase: path.resolve(__dirname, "dist")
-  }
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, "src", "manifest.json"),
+        transform: function(content, path) {
+          // generates the manifest file using the package.json informations
+          return Buffer.from(
+            JSON.stringify({
+              description: process.env.npm_package_description,
+              version: process.env.npm_package_version,
+              ...JSON.parse(content.toString())
+            })
+          );
+        }
+      }
+    ])
+  ]
 };
